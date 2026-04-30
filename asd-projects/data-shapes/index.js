18eded8f2e05d3373b69469b9f8d8533a1e44155
Andreycooper22 <andrey.cooper.student@madisonpsb.org>
2026-04-30 19:31:52 +0000

    datashapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
     shape: "circle",
     repeat: 3
   }
-
+dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
    for (let i = 0; i < dataShapes.length; i++){
     let currentShape = dataShapes[i];
@@ -58,13 +58,13 @@ $(document).ready(function () {
   // TODO 3-a: add a function that handles the static display type
    function handleStatic(data) {
     setBackgroundWithObject(data);
-    animalsDetails.displayType = 1;
-
+    animationDetails.displayType = 1;
+   }
   // TODO 4-a: add a function that handles the good display type
-    function handleGood(color, shape, repeat){
+    function handleGood(color, shape, repeat) {
     setBackgroundWithSimple(color, shape, repeat);
-    animalsDetails.displayType = 2;
-
+    animationDetails.displayType = 2;
+    }
   // TODO 5-a: add a function that handles the bad display type
    function handleBad(data, repeat) {
   repeat = repeat + 1;
